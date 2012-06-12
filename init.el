b93d0f8bc17742aefd0f841a1abf63819a0390a2
Robert Chu <robert@wibidata.com>
2012-06-11 19:42:00 -0700

    Set google-chrome as default browser, set fill-column to 100, added log-entry inserting function.

--- init.el
+++ init.el
@@ -9,6 +9,15 @@
   kept-new-versions 20   ; how many of the newest versions to keep
   kept-old-versions 5    ; and how many of the old
   )
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "google-chrome")
+(setq-default fill-column 100)
+
+;;; Define cool functions
+(defun log-entry ()
+  "Insert an log entry header logging the current date/time"
+  (interactive)
+  (insert (format-time-string "[Entry on %A, %B %e %Y (%r)]: ")))
 
 ;;; Load plugins
 (load "~/.emacs.d/available-plugins.el")
